475c99a9ade111e138c3bf564dfccc96871ed9d1
Josh Bayer <bayerj@gmail.com>
2022-10-22 09:19:32 -0400

    added response entity

--- src/main/java/com/joshwork/pokedex/controller/BucketController.java
+++ src/main/java/com/joshwork/pokedex/controller/BucketController.java
@@ -2,6 +2,8 @@ package com.joshwork.pokedex.controller;
 
 import com.joshwork.pokedex.service.AmazonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,8 +19,8 @@ public class BucketController {
     }
 
     @PostMapping("/uploadFile")
-    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
-        return this.amazonClient.uploadFile(file);
+    public ResponseEntity<String> uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.amazonClient.uploadFile(file));
     }
 
 }
